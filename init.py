81d1e80074d0cefb1e18d7341058a930c765b22c
Peter de Groot <peter.de.groot@vgz.nl>
2020-11-17 14:09:04 +0100

    Rudimentary MsSql class added

--- init.py
+++ init.py
@@ -1,9 +1,39 @@
-import os
 
-mssql={}
-for k in ( key for key in os.environ.keys() if key.startswith("MSSQL_")):
-    mssql[ k[6:] ] = os.environ.get(k)
-    print(f"mssql[\"{k[6:]}\"]={mssql[ k[6:] ]}")
+class MsSql():
+    engine = None
+    conf = {}
 
-print(str(mssql))
+    def __init__(self):
+
+        import os
+        for k in ( key for key in os.environ.keys() if key.startswith("MSSQL_")):
+            self.conf[ k[6:] ] = os.environ.get(k)
+
+    def set_server(self, server):
+        self.conf["server"] = server
+
+    def set_database(self, database):
+        self.conf["password"] = database
+
+    def set_username(self, username):
+        self.conf["password"] = username
+
+    def set_password(self, pw):
+        self.conf["password"] = pw
+
+    def engine(self):
+        if self.engine == None:
+            import urllib.parse
+            connectstring = urllib.parse.quote_plus(
+                "DRIVER=ODBC Driver 17 for SQL Server;SERVER={0};PORT=1433;DATABASE={1};"
+                "UID={2};PWD={3};".format(
+                    self.conf["server"], self.conf["database"], self.conf["username"], self.conf["password"]
+                )
+            )
+            dburl = "mssql+pyodbc:///?odbc_connect={}".format(connectstring)
+            self.engine = create_engine(dburl, pool_pre_ping=True)
+
+        return self.engine
+
+mssql = MsSql()
 
